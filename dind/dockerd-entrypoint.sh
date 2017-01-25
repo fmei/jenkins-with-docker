5df9d56c53774f563768914e51f1a6736557884d
Dominik Urban <UrbanD@external.t-systems.com>
2017-01-25 18:13:23 +0100

    added prerequisites for storage driver

--- dind/dockerd-entrypoint.sh
+++ dind/dockerd-entrypoint.sh
@@ -8,6 +8,7 @@ if [ "$#" -eq 0 -o "${1:0:1}" = '-' ]; then
 		--host=unix:///var/run/docker.sock \
 		--host=tcp://0.0.0.0:2375 \
 		--storage-driver=devicemapper \
+		--storage-opt dm.override_udev_sync_check=true \
 		"$@"
 fi
 
